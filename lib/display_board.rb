45520f4038e4e2592a2d90c39d199818d4ebe06c
Shiping Yi <esp132@gmail.com>
2018-02-19 05:15:07 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-Def display_board()
-puts "   |   |   "
+Def display_board(boards)
+puts "#{boards[0]}|#{boards[1]}|#{boards[2]}"
 puts "-----------"
-puts "   |   |   "
+puts "#{boards[3]}|#{boards[4]}|#{boards[5]}"
 puts "-----------"
-puts "   |   |   "
+puts "#{boards[6]}|#{boards[7]}|#{boards[8]}"
 end
